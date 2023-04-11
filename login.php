5fa7142734536a596f1ad933cbb770711ea3fce6
madison_pierre <mpierre@stedwards.edu>
2023-04-10 19:39:57 -0500

    updated paths and named main files index

--- login.php
+++ login.php
@@ -37,7 +37,7 @@
 			//get the user's clearance
 			$clearance_q = "SELECT clearance FROM UserAccounts WHERE username=$user_username";
 			$clearance = $conn->query($clearance_q);
-			header("Location:dashboard.html");
+			header("Location:Files\dashboard.html");
 		}
 		else {
 			//this is the wrong username/password combo
