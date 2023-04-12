1d3b12a6dc2e5669e331d5a43ad2c6bcd263ff20
madison_pierre <mpierre@stedwards.edu>
2023-04-12 08:44:49 -0500

    captcha is required field

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
 
 	<body>
 	<form action="Files\login.php" method="post">
-	<label for="username"> Username :</label>
-	<input type="text" name="username"/>
+	<label for="username" > Username :</label>
+	<input type="text" name="username" required />
 	<br>
 	<label for="psswrd">Password :</label>
-	<input type="password" name="psswrd"/>
+	<input type="password" name="psswrd" required />
 	<div>
 	<label>Please do the captcha to prove you're not a bot:</label>
 	<?php
@@ -18,7 +18,7 @@
 	  $PHPCAP->prime();
 	  $PHPCAP->draw();
 	  ?>
-  <input name="captcha" type="text" required>
+  <input name="captcha" type="text" required />
 	</div>
 	
 	<button type="submit"> Sign In </button>
