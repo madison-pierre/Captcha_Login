639ca2e6752c04da0f9183c7c86670a23e81d9f6
madison_pierre <mpierre@stedwards.edu>
2023-04-13 18:07:49 -0500

    fixed wrong password reference

--- login.php
+++ login.php
@@ -16,7 +16,7 @@
 		$user_username = $_POST["username"];
 		$user_psswrd = $_POST["psswrd"];
 		// hash password with md5
-		$hash_password = md5($psswrd, false);
+		$hash_password = md5($user_psswrd, false);
 		echo "$user_username $hash_password";
 
 		//check that username and password are valid
