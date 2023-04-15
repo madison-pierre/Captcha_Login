f1e5e716fb3098871c8813a8741c4e81942db9e5
madison_pierre <mpierre@stedwards.edu>
2023-04-15 12:51:19 -0500

    fixed folders 3

--- login.php
+++ login.php
@@ -3,89 +3,6 @@
 <link rel="stylesheet" href="hw5.css">
 </head>
 <?php
-/*
-	
-	
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		//first check that captcha passes
-		// LOAD CAPTCHA 
-		require "hw5_captcha.php";
- 
-		// (B) Verify if its corrext or not
-		if ($PHPCAP->verify($_POST["captcha"])) {
-		  $result = "Congrats, CAPTCHA is correct.";
-		  print_r($_POST);
-		}
-		else { echo "CAPTCHA does not match!"; }
-		
-		// then do other login stuff
-		$user_username = $_POST["username"];
-		$user_psswrd = $_POST["psswrd"];
-		// hash password with md5
-		$hash_password = md5($user_psswrd, false);
-		// echo "$user_username $hash_password";
-
-		//check that username and password are valid
-		
-		//log into DB
-		
-		
-		$servername = "localhost";
-		$username = "accessor";
-		$password = "phpIsNoFun";
-		$db_name = "SiteDatabase";
-		//make connection
-		$conn = new mysqli($servername, $username, $password, $db_name);
-		//echo $conn;
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-			echo "BAD CONNECTION";
-		}
-		else {
-			echo " Connected successfully ";
-			mysqli_connect_error($conn);
-			//see if the entered username and password match an entry in the DB
-			//test query
-			$test_q ="SELECT * FROM UserAccounts";
-			$test_result = $conn->query($test_q);
-			if (!$test_result) {
-			echo " QUERY DID NOT FAIL SEE $test_result "; //it really doesn't like test result
-			}
-			else {
-				echo " QUERY ISN'T WORKING CANNOT PRINT TEST RESULT ";
-			}
-		}
-		
-
-		
-		$result = "";
-		$query = $conn->prepare("SELECT clearance FROM UserAccounts WHERE password=? AND username=?");
-		$query->bind_param('ss',$hash_password,$user_username);
-		$query->execute();
-		$query->bind_result($result);
-		// echo $query; We get to this statement then stop		
-		echo " $result "; //result does not echo, is it because nothing is there?
-		
-		//if they are found, display the images on new html file, if not, display error
-		if ($result->num_rows > 0) { //successful log in
-			//get the user's clearance
-			$clearance = $result;
-			echo("<p>Redirecting...<p>");
-			$conn->close();	
-			header("Location:dashboard.php");	
-		} 
-		else {
-			//this is the wrong username/password combo
-			echo "<p>**You've entered the wrong username/password. Please try again**</p>";
-			echo "<button action='index.php'> RETRY </button>";
-			}
-	  }
-	else {
-		echo "<p>Something went wrong, no POST detected</p>";
-	}
-	
-	
-*/
 
 session_start();
 
@@ -128,6 +45,7 @@ if(isset($_POST['username']))
     $account = $result->fetch_assoc();
 	//getting the clearance
 	$user_clearance = $account["clearance"];
+	echo $user_clearance;
 /*	
 	//now we display the images
 	//create a list of them
